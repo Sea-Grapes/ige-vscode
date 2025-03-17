e3d5043aec62ee8cf4ba537e66c9d10d30e3eb71
Sea Grapes <natuloid@gmail.com>
2025-03-16 19:44:25 -0700

    bug: document manager incremental sync non-functional

--- src/server.ts
+++ src/server.ts
@@ -49,29 +49,28 @@ ws.onDidChangeTextDocument((data: any) => {
   data.contentChanges.forEach(change => {
     if(change.range) {
       const { start, end } = change.range
-      const before = doc[start.line].slice(0, start.character)
-      const after = doc[end.line].slice(end.character)
+      let before = doc[start.line]? doc[start.line].slice(0, start.character): null
+      let after = doc[end.line]? doc[end.line].slice(end.character): null
       const newLines = change.text.split(rgx_newline)
 
       if(start.line === end.line && newLines.length === 1) {
         // console.log('same line insertion')
         // console.log(before, newLines[0], after)
-        doc[start.line] = before + newLines[0] + after
+        let res = newLines[0]
+        if(before) res = before + res
+        if(after) res += after
+        doc[start.line] = res
       } else {
-        console.log('end: ', end.line)
-        console.log('start: ', start.line)
-        console.log(`splicing at ${start.line}, removing ${end.line - start.line + 1} lines`)
-        console.log(`inserting "${before + newLines[0]}", "${newLines.at(-1) + after}" and the following: `, ...newLines.slice(1, -1))
-
-        let newBefore = before + newLines[0]
-        let newAfter = newLines.at(-1) + after
+        let res = []
+        
+        if(before) res.push(before + newLines[0])
+        res.push(...newLines.slice(1, -1))
+        if(after) res.push(newLines.at(-1) + after)
 
         doc.splice(
           start.line,
           end.line - start.line + 1,
-          before + newLines[0],
-          ...newLines.slice(1, -1),
-          newLines.at(-1) + after
+          ...res
         )
       }
 
@@ -81,7 +80,8 @@ ws.onDidChangeTextDocument((data: any) => {
     }
   })
 
-  console.log(doc)
+  console.log('doc update')
+  console.log(doc.join('\n'))
 })
 
 ws.onDidCloseTextDocument(data => {
